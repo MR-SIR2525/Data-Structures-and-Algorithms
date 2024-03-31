2f3c499ac31baa8e85046c42a2eefa6eeb019e1f
MR-SIR2525 <95254450+MR-SIR2525@users.noreply.github.com>
2024-03-30 20:14:41 -0500

    Challenge 02 stuff

---
+++ Challenge02 demo/Queue.cpp
@@ -0,0 +1,59 @@
+#include "Queue.h"
+
+Queue::~Queue()
+{
+    Node *tmp = first;
+
+    while (tmp != nullptr)  //hits nullptr when Queue is no more
+    {
+        first = first->next;
+        delete tmp;
+        
+        tmp = first;
+    }
+    first = last = nullptr;
+}
+
+void Queue::push(int data) //append now...
+{
+    Node *newnode = new Node(data);
+
+    if (first == nullptr)
+        first = newnode;
+    else
+        last->next = newnode;
+    last = newnode;
+}
+
+bool Queue::pop()
+{   
+    if (first == nullptr) return false;   //empty Queue, pop fails
+
+    Node *current = first;
+    first = first->next;    // set first to the node beneath itself
+
+    if (first == nullptr)
+        last = nullptr;
+
+    delete current;         //OG first deleted
+    return true;
+}
+
+bool Queue::peek(int &data)
+{
+    if (first == nullptr) return false;   //Queue empty, can't peek
+    data = first->data;
+    return true;
+}
+
+void Queue::display(std::ostream &os)
+{
+    Node *current = first;      // start from the first
+
+    while (current != nullptr)      //stops when current hits 'tail'.
+    {
+        os << current->data << " ";
+        current = current->next;    //move forwards
+    }
+    os << "\n"; 
+}
